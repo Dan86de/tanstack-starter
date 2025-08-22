0825fbf41ee5b7875b989e7234ce74fe7f0311f8
Daniel Noworyta <daniel.noworyta@gmail.com>
2025-08-22 07:54:24 +0200

    Update center examples

--- src/routes/center.tsx
+++ src/routes/center.tsx
@@ -6,33 +6,57 @@ export const Route = createFileRoute("/center")({
 
 function RouteComponent() {
 	return (
-		<div className="inset-0 h-screen">
+		<div className="stack-v-8 inset-0 h-screen">
 			{/* Basic centered container */}
 			<div className="center">
-				<p>Content centered with optimal reading width</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+					dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit
+					amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.
+				</p>
 			</div>
 
 			{/* Centered with custom max-width and gutters */}
 			<div className="center-4xl center-gutter-4">
-				<p>Wider content with padding on sides</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+					dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit
+					amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.
+				</p>
 			</div>
 
 			{/* Centered with text alignment */}
 			<div className="center center-text">
 				<h1>Centered Heading</h1>
-				<p>Centered paragraph text</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+					dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit
+					amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.
+				</p>
 			</div>
 
 			{/* Intrinsic centering for variable-width children */}
-			<div className="center center-intrinsic">
-				<button>Short</button>
-				<button>A much longer button</button>
+			<div className="center center-intrinsic stack-v-2">
+				<button className="box-2 cursor-pointer transition-all duration-200 hover:bg-[var(--foreground)] hover:text-[var(--background)]">
+					Short
+				</button>
+				<button className="box-2 cursor-pointer transition-all duration-200 hover:bg-[var(--foreground)] hover:text-[var(--background)]">
+					A much longer button
+				</button>
 			</div>
 
 			{/* Combine utilities for complex layouts */}
 			<article className="center-3xl center-gutter-6 center-text center-intrinsic">
 				<h1>Article Title</h1>
-				<p>Content that's fully centered</p>
+				<p>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Lorem ipsum
+					dolor sit amet, consectetur adipiscing elit. Lorem ipsum dolor sit
+					amet, consectetur adipiscing elit. Lorem ipsum dolor sit amet,
+					consectetur adipiscing elit.
+				</p>
 			</article>
 		</div>
 	);
